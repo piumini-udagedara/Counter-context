2c9896387b0125c3d5f3818191fc5088f00d3d2f
Piumini Udagedara <piuminiudagedara@Piuminis-MBP.localdomain>
2023-04-06 09:43:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import { useCount } from "./counter-context";
 
-function App() {
+function CounterController() {
+  const { dispatch } = useCount();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => dispatch({ type: "increment" })}>Incrment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+    </>
   );
 }
 
-export default App;
+function CountDisplay() {
+  const {
+    state: { count }
+  } = useCount();
+  return <div>{count}</div>;
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      count
+      <CountDisplay />
+      <CounterController />
+    </div>
+  );
+}
